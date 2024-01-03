9a472e6a83a11fc2351a267f9553ec249e1d65c9
KGaraj <146760989+KGaraj@users.noreply.github.com>
2024-01-03 09:28:17 +0100

    Creating a function for scraping data for single item
    Creating a function for scraping data for single item with variables which will allow us to use the df in scrape_all_skins.py to use this function for all the weapons in the df

--- units/scrape_single_item.py
+++ units/scrape_single_item.py
@@ -1,130 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
-import matplotlib.pyplot as plt
-#creating an empty array for the price converter to append to
-array = []
-def converter(file):  #note that one singular formating can't be used for all currencies because each of them is formated differently
-    for j in file:
-        for i in j:
-            if i.startswith("$") & i.endswith("USD"):
-                i = i.replace("$", "")
-                i = i.replace("USD", "")
-                i = float(i)
-                print("$",round(i,2),"USD")
-            elif i.endswith("₴"):
-                i = i.replace("₴" , "")
-                i = i.replace(",", ".")
-                i = i.replace(" ", "")
-                i = float(i)
-                i = i*0.027429796
-                print("$",round(i,2),"USD")
-            elif i.startswith("¥ "):
-                i = i.replace("¥ ", "")
-                i = i.replace(",", "")
-                i = float(i)
-                y = i*0.006756441
-                i = i*0.14055287
-                print("$",round(i,2),"USD or", round(y,2),"USD")
-            elif i.startswith("₩ "):
-                i = i.replace("₩ ", "")
-                i = i.replace(",", "")
-                i = float(i)
-                i = i*0.00076791519
-                print("$",round(i,2),"USD")         
-            elif i.startswith("CHF "):
-                i = i.replace("CHF ", "")
-                i = i.replace("--","00")
-                i = float(i)
-                i = i*1.1448816
-                print("$",round(i,2),"USD")     
-            elif i.endswith("₸"):
-                i = i.replace("₸","")
-                i = i.replace(",", ".")
-                i = i.replace(" ", "")
-                i = float(i)
-                i = i*0.0021677483
-                print("$",round(i,2),"USD")  
-            elif i.endswith("pуб."):
-                i = i.replace("pуб.","")
-                i = i.replace(",", ".")
-                i = float(i)
-                i = i*0.011110017
-                print("$",round(i,2),"USD")  
-            elif i.endswith("€"):
-                i = i.replace("€","")
-                i = i.replace(",", ".")
-                i = i.replace("--","00")
-                i = float(i)
-                i = i*1.0900791
-                print("$",round(i,2),"USD") 
-            elif i.startswith("£"):
-                i = i.replace("£", "")
-                i = float(i)
-                i = i*1.2644539
-                print("$",round(i,2),"USD")
-            elif i.startswith("฿"):
-                i = i.replace("฿", "")
-                i = float(i)
-                i = i*0.028504166
-                print("$",round(i,2),"USD")
-            elif i.endswith("₫"):
-                i = i.replace("₫","")
-                i = i.replace(".", "")
-                i = float(i)
-                i = i*0.000041200184
-                print("$",round(i,2),"USD")
-            elif i.startswith("₹ "):
-                i = i.replace("₹ ", "")
-                i = i.replace(",", "")
-                i = float(i)
-                i = i*0.012000527
-                print("$",round(i,2),"USD")
-            elif i.endswith(" TL"):
-                i = i.replace(" TL","")
-                i = i.replace(".", "")
-                i = i.replace(",", ".")
-                i = float(i)
-                i = i*0.03459303
-                print("$",round(i,2),"USD")
-            elif i.startswith("CDN$ "):
-                i = i.replace("CDN$ ", "")
-                i = float(i)
-                i = i*0.7392775
-                print("$",round(i,2),"USD")
-            elif i.endswith(" kr"):
-                i = i.replace(" kr","")
-                i = i.replace(".", "")
-                i = i.replace(",", ".")
-                i = float(i)
-                i = i*0.095753654
-                print("$",round(i,2),"USD")
-            elif i.startswith("A$ "):
-                i = i.replace("A$ ","")
-                i = float(i)
-                i = i*0.66183303
-                print("$",round(i,2),"USD")
-            elif i.startswith("R$ "):
-                i = i.replace("R$ ","")
-                i = i.replace(",",".")
-                i = float(i)
-                i = i*0.20316714
-                print("$",round(i,2),"USD")
-            else:
-                print("I don't know that yet")
-        global array        
-        array= np.append(array,i) #appending the result of price conversion into a np array
 
+def scrape_item(name_part1,name_part2, weapon): #creating a function for scraping a item from the steam market with variables
+    url = "https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Quality%5B%5D=tag_normal&appid=730&q="+str(weapon)+"+%7C+"+str(name_part1)+"+"+str(name_part2)
 
-url = "https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Blue%20Laminate%20%28Factory%20New%29" 
-#note that the url includes the name of the skin and is not randomly generated which means that if we have all the skin names we can search each of them
+    # Scraping the prices and conditions from the Steam market
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, features="html.parser")
 
-#scraping the prices from the steam market
-request = requests.get(url)
-soup = BeautifulSoup(request.text, features="html.parser")
-prices = soup.find_all("span", {"class":"market_listing_price_with_fee" })
-prices_text = np.array(prices)
-prices_clean = np.char.strip(prices_text, "\r\n\t\t\t\t\t\t")
-
-#using the above creatid price converter on the prices scraped from the steam market
-converter(prices_clean)
+    items = soup.find_all("div", class_="market_listing_row")
+    for item in items:
+        item_name = item.find("span", class_="market_listing_item_name").get_text(strip=True) 
+        condition = item_name.split('(')[-1].strip(')')  # Extracting conditions
+        prices = item.find_all("span", class_="sale_price") #Extracting prices
+        prices_text = np.array(prices) #Creating a numpy array from the prices
+        prices_clean = np.char.strip(prices_text, "\r\n\t\t\t\t\t\t") #Cleaning the exctracted values
+    
+        # Printing conditions and coresponding prices
+        print(condition+":")
+        print(prices_clean)
+        #note that these are sale prices, eg an ammount that seller receives after the sale
+    
+scrape_item("blue","laminate","ak-47")
