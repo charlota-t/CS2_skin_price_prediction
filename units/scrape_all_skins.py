e3eef2ff6c1d455a6d631d98ab5b6dca977baa9f
KGaraj <146760989+KGaraj@users.noreply.github.com>
2024-01-03 11:56:45 +0100

    scrape_all_skins now pulls data for all skins
    scrape_all_skins now scrapes prices for all skins and weapons in the constructed dataframe

--- units/scrape_all_skins.py
+++ units/scrape_all_skins.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
+import time
+import random
 
 #frequently updated url with the list of all skins 
 url = "https://counterstrike.fandom.com/wiki/Skins/List"
@@ -53,5 +55,49 @@ weapons_array = np.array(list2)
 df = pd.DataFrame()
 df['Weapon type'] = pd.Series(weapons_array)
 df['Skin'] = pd.Series(skins_array)
-print(df)
+#print(df)
+
+#for i in df["Skin"]:
+#    for j in df["Weapon type"]
+#    print(j, len(j.split()), i, len(i.split()))
+def create_url(name_parts, weapon_parts):
+    weapon = '+'.join(weapon_parts)
+    skin = '+'.join(name_parts)
+
+    # Constructing the URL
+    url = f"https://steamcommunity.com/market/search?category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Quality%5B%5D=tag_normal&appid=730&q={weapon}+%7C+{skin}"
+    return url
+
+def request_all():
+    for i in range(0,len(df)):
+        weapon_type = df.loc[i,"Weapon type"] 
+        skin_name = df.loc[i,"Skin"]
+                
+        weapon_words = weapon_type.split() #spliting the weapon_type and skin_name into separate words to construct the url
+        skin_words = skin_name.split()
+        
+
+        # Constructing URL based on the number of words
+        url = create_url(skin_words, weapon_words)
+        print(url)
+        #Scraping the prices and conditions from the Steam market
+        request = requests.get(url)
+        soup = BeautifulSoup(request.text, features="html.parser")
+
+        items = soup.find_all("div", class_="market_listing_row")
+        for item in items:
+            item_name = item.find("span", class_="market_listing_item_name").get_text(strip=True) 
+            condition = item_name.split('(')[-1].strip(')')  # Extracting conditions
+            prices = item.find_all("span", class_="sale_price") #Extracting prices
+            prices_text = np.array(prices) #Creating a numpy array from the prices
+            prices_clean = np.char.strip(prices_text, "\r\n\t\t\t\t\t\t") #Cleaning the exctracted values
+    
+            # Printing conditions and coresponding prices
+            print(condition+":")
+            print(prices_clean)
+            #note that these are sale prices, eg an ammount that seller receives after the sale
+        time.sleep(random.uniform(6,7)) #wait 6 to 7 seconds due to request rate limiter on steam market
+
+request_all()
+
 
