3b9ddc0a3140714f4789d5eadd2aa25190d003fb
KGaraj <146760989+KGaraj@users.noreply.github.com>
2024-01-03 19:47:47 +0100

    Adding a scrape_fandom.py module
    Adding a scrape_fandom.py module that is used by main.py that creates a pandas data frame from the data pulled from fandom

---
+++ main/scrape_fandom.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def scrape():    
+#frequently updated website with all weapon skin names
+    url = "https://counterstrike.fandom.com/wiki/Skins/List"
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, features="html.parser")
+    all_tables = soup.find_all("table", class_="wikitable") #we search for tables in the website as they contain all the info we need
+
+    current_collection = "NaN"  #if we are not able to find the collection
+    skins_info = [] #initializing skins_info
+    for i in all_tables: 
+    # find collection name above the table
+        last_span = i.find_previous("span", class_="mw-headline")
+        if last_span:
+            current_collection = last_span.text.strip() 
+
+        rows = i.find_all("tr")
+        for j in rows[1:]:
+            columns = j.find_all(["td", "th"])
+            if len(columns) >= 3: #making sure we only take valid rows
+                skin_name = columns[1].get_text(strip=True)
+                quality = columns[2].get_text(strip=True)
+                weapon_type = columns[0].get_text(strip=True)
+                skins_info.append([weapon_type, skin_name, quality, current_collection])
+#creating the df with the corresponding column names
+
+    df = pd.DataFrame(skins_info, columns=["Weapon type", "Skin", "Quality", "Collection"])
+    return(df) #we use return so that we can pass this variable to main
+#print(df) if used as a unit 
