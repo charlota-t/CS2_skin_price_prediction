042a01276503c6dddda365bb8f6df301fb0850a6
KGaraj <146760989+KGaraj@users.noreply.github.com>
2024-01-04 12:06:15 +0100

    more efficient scrape_all_skins.py
    Making scrape_all_skins.py scrape  values for the conditions all at once allowing us to make fewer requests. adding delay between requests due to request rate limiter

--- units/scrape_all_skins.py
+++ units/scrape_all_skins.py
@@ -5,63 +5,35 @@ import pandas as pd
 import time
 import random
 
-#frequently updated url with the list of all skins 
-url = "https://counterstrike.fandom.com/wiki/Skins/List"
-request = requests.get(url)
-soup = BeautifulSoup(request.text, features="html.parser")
+def scrape():
+    url = "https://counterstrike.fandom.com/wiki/Skins/List"
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, features="html.parser")
+    all_tables = soup.find_all("table", class_="wikitable") #we search for tables in the website as they contain all the info we need
 
-qualities = ("common","uncommon", "rare", "mythical", "legendary","ancient") #CS2 qualities (does not inqlude knifes and gloves as they are too unpredictable in price)
-not_wanted = ("Consumer Grade", "Industrial Grade", "Mil-Spec", "Restricted", "Classified","Covert" )
-#CS2 weapons, does not change
-all_weapons = ("Desert Eagle" , "R8 Revolver", "Dual Berettas", "Five-SeveN", "Glock-18", "P2000" ,"USP-S", "P250", "CZ75-Auto", "Tec-9", "MAG-7", "Nova" ,"Sawed-Off", "XM1014", "PP-Bizon", "MAC-10", "MP7", "MP5-SD", "MP9", "P90", "UMP-45", "AK-47" ,"AUG", "FAMAS", "Galil AR", "M4A4", "M4A1-S", "SG 553", "M249", "Negev", "AWP", "G3SG1", "SCAR-20", "SSG 08")
+    current_collection = "NaN"  #if we are not able to find the collection
+    skins_info = [] #initializing skins_info
+    for i in all_tables: 
+    # find collection name above the table
+        last_span = i.find_previous("span", class_="mw-headline")
+        if last_span:
+            current_collection = last_span.text.strip() 
 
-#creating an empty list to add the skin names scraped from the url to
-list = []
-#scraping the site for the skins names and removing unwanted scraped elements
-items = soup.find_all("span", {"class":qualities}) 
-for j in items:
-    for i in j:  
-        if i == "Consumer Grade":
-            pass
-        elif i == "Industrial Grade":
-            pass
-        elif i == "Mil-Spec":
-            pass
-        elif i == "Restricted":
-            pass
-        elif i == "Classified":
-            pass
-        elif i == "Covert":
-            pass
-        elif i == "x":
-            pass
-        elif str(i) == '<b>*</b>':
-            pass
-        else:
-         list.append(str(i))
-#converting created list into np array
-skins_array = np.array(list)
-#scraping the site for the weapon names
-weapons = soup.find_all("a", {"title":all_weapons }) 
-#creating list2 to add the weapon names scraped from the url to
-list2 = []
-#scraping the weapon names
-for j in weapons:
-    for i in j:
-       list2.append(str(i)) 
-#converting list2 into array
-weapons_array = np.array(list2)
-#creating a pd dataframe from list and list2
-df = pd.DataFrame()
-df['Weapon type'] = pd.Series(weapons_array)
-df['Skin'] = pd.Series(skins_array)
-#print(df)
+        rows = i.find_all("tr")
+        for j in rows[1:]: #skipping the first row as it contains the header of the table
+            columns = j.find_all(["td", "th"])
+            if len(columns) >= 3: #making sure we only take valid rows
+                skin_name = columns[1].get_text(strip=True) 
+                quality = columns[2].get_text(strip=True)
+                weapon_type = columns[0].get_text(strip=True)
+                skins_info.append([weapon_type, skin_name, quality, current_collection])
+#creating the df with the corresponding column names
+    global df
+    df = pd.DataFrame(skins_info, columns=["Weapon type", "Skin", "Quality", "Collection"])
+scrape()
 
-#for i in df["Skin"]:
-#    for j in df["Weapon type"]
-#    print(j, len(j.split()), i, len(i.split()))
-def create_url(name_parts, weapon_parts):
-    weapon = '+'.join(weapon_parts)
+def create_url(name_parts, weapon_parts): #creating a function to build the url from the information in the dataframe
+    weapon = '+'.join(weapon_parts) #joining name_parts and weapon_parts with a + symbol which is needed for the url
     skin = '+'.join(name_parts)
 
     # Constructing the URL
@@ -72,9 +44,9 @@ def request_all():
     for i in range(0,len(df)):
         weapon_type = df.loc[i,"Weapon type"] 
         skin_name = df.loc[i,"Skin"]
-                
-        weapon_words = weapon_type.split() #spliting the weapon_type and skin_name into separate words to construct the url
-        skin_words = skin_name.split()
+        #spliting the weapon_type and skin_name into separate words to construct the url        
+        weapon_words = weapon_type.split() 
+        skin_words = skin_name.split() 
         
 
         # Constructing URL based on the number of words
@@ -87,17 +59,15 @@ def request_all():
         items = soup.find_all("div", class_="market_listing_row")
         for item in items:
             item_name = item.find("span", class_="market_listing_item_name").get_text(strip=True) 
-            condition = item_name.split('(')[-1].strip(')')  # Extracting conditions
-            prices = item.find_all("span", class_="sale_price") #Extracting prices
-            prices_text = np.array(prices) #Creating a numpy array from the prices
+            condition = item_name.split("(")[-1].strip(")")  # extracting conditions 
+            prices = item.find_all("span", class_="sale_price") #extracting prices for all conditions at once which saves us requests
+            prices_text = np.array(prices) #creating a numpy array from the prices
             prices_clean = np.char.strip(prices_text, "\r\n\t\t\t\t\t\t") #Cleaning the exctracted values
     
             # Printing conditions and coresponding prices
             print(condition+":")
             print(prices_clean)
             #note that these are sale prices, eg an ammount that seller receives after the sale
-        time.sleep(random.uniform(6,7)) #wait 6 to 7 seconds due to request rate limiter on steam market
+        time.sleep(random.uniform(8,9)) #wait 8 to 9 seconds due to request rate limiter on steam market
 
 request_all()
-
-
