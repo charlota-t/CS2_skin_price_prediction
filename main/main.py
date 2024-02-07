c77e8936a2d2b1095ec5bd606d54c31421f50b56
KGaraj <146760989+KGaraj@users.noreply.github.com>
2024-02-07 16:28:44 +0100

    Changes to modules reflected in main.py
    Making main.py reflect changes in certain modules

--- main/main.py
+++ main/main.py
@@ -1,6 +1,6 @@
 import scrape_fandom
 import scrape_skin_prices as ssp
-import scrape_skin_prices_modules as sspm
+
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
@@ -14,5 +14,4 @@ df = scrape_fandom.build_df(skins_info)
 
 df.to_csv("allvalues.csv", index=False)
 
-sspm.read_all(df)
-#ssp.request_all(df)
+ssp.read_all(df)
